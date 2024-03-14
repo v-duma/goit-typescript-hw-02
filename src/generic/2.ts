ce9c12bb3630542e103a79d0baed35fb8306dc52
v-duma <139060657+v-duma@users.noreply.github.com>
2024-03-15 00:26:00 +0200

    updated files

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,10 +11,10 @@ type AllType = {
   weight: number;
 };
 
-function compare<T extends AllType, U extends AllType>(
-  top: Pick<T, keyof AllType>,
-  bottom: Pick<U, keyof AllType>
-): AllType {
+function compare<
+  T extends Pick<AllType, "color" | "name">,
+  U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
